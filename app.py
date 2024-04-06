6ea279eeeadb67e9e731659250b2a17e552fb193
LalasaMynalli <164126898+LalasaMynalli@users.noreply.github.com>
2024-04-06 12:49:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -37,10 +37,19 @@ occ_options = [' Adm support including clerical', ' Sales',
        ' Machine operators assemblers & inspectors', ' Armed Forces']
 tax_options = [' Nonfiler', ' Joint one under 65 & one 65+', ' Single',
        ' Joint both under 65', ' Head of household', ' Joint both 65+']
+edu_options = [' High school graduate', ' 12th grade no diploma', ' Children',
+       ' Bachelors degree(BA AB BS)', ' 7th and 8th grade', ' 11th grade',
+       ' 9th grade', ' Masters degree(MA MS MEng MEd MSW MBA)',
+       ' 10th grade', ' Associates degree-academic program',
+       ' 1st 2nd 3rd or 4th grade', ' Some college but no degree',
+       ' Less than 1st grade', ' Associates degree-occup /vocational',
+       ' Prof school degree (MD DDS DVM LLB JD)', ' 5th or 6th grade',
+       ' Doctorate degree(PhD EdD)']
 
 with st.form('Prediction form'):
     st.header('Enter the people specifications:')
     age = st.number_input (label='Age: ',min_value = 0, max_value = 90, step = 1)
+    education = st.selectbox ('Select the type of education: ',options = edu_options)
     employment_commitment = st.selectbox('Select the type of employment: ',options = employment_options)
     gender = st.selectbox('Select gender: ',options= gender_options)
     industry_code_main = st.selectbox('Select the type of industry: ',options = industry_options)
@@ -48,10 +57,13 @@ with st.form('Prediction form'):
     mig_year = st.number_input (label = 'Migration year: ',min_value = 94, max_value = 95, step = 1)
     occupation_code_main = st.selectbox('Select the type of occupation: ', options = occ_options)
     tax_status = st.selectbox ('Select the type of tax filer: ', options = tax_options)
+    working_week_per_year = st.number_input (label='No. of working week per year: ',min_value = 1, max_value = 52, step = 1)
+   
     
     submit_values = st.form_submit_button ('Predict')
     
 if submit_values:
+    education = ordinal_encoder(education,edu_options)
     employment_commitment = ordinal_encoder(employment_commitment,employment_options)
     gender = ordinal_encoder(gender,gender_options)
     industry_code_main = ordinal_encoder(industry_code_main,industry_options)
@@ -59,8 +71,8 @@ if submit_values:
     occupation_code_main = ordinal_encoder(occupation_code_main,occ_options)
     tax_status = ordinal_encoder(tax_status,tax_options)
     
-    data = np.array([age,employment_commitment,gender,industry_code_main,marital_status,
-                     mig_year,occupation_code_main,tax_status]).reshape(1,-1)
+    data = np.array([age,education, employment_commitment,gender,industry_code_main,marital_status,
+                     mig_year,occupation_code_main,tax_status, working_week_per_year]).reshape(1,-1)
     
     prediction = predict(data)
     
